40384449fd03423ff03e15f75d811851e32c6164
Sebastian Diaz <Sebastian Diaz@DESKTOP-N2EHBFE>
2023-07-05 16:45:06 -0500

    Primer commit de agenda electronica.

---
+++ src/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,15 @@
+package agendaelectronica;
+
+import agendaelectronica.igu.Principal;
+import java.util.Locale;
+
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+
+    }
+    
+}
